78b7021fe65e24812b8d3dc28d39bb78f9db4289
Orkhan Jafarov <orkhanjafarovr@gmail.com>
2018-06-19 03:55:00 +0400

    init

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import ReactDOM from 'react-dom';
+
+import Xash from './xash';
+const xash = new Xash();
+
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  flexDirection: 'column',
+  margin: '0 auto',
+  maxWidth: '1024px',
+  width: '100%',
+  padding: 32
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: localStorage.getItem('text') || 'Text text text  text text',
+    }
+  }
+
+  _inputHandler = (e) => {
+    const text = e.currentTarget.value;
+    
+    this.setState({ text }, () => {
+      localStorage.setItem('text', this.state.text);
+    });
+  }
+
+  render() {
+    const { text } = this.state;
+
+    return (
+      <div style={style}>
+        <h1>React-Xash</h1>
+        <p>
+          Utility for adding hash into your text to protect your content.
+        </p>
+
+        <br/>
+      
+        <textarea value={text} onChange={this._inputHandler} style={{ width: '100%', height: '15vh' }} />
+        <p>
+          <b>Result</b><br />
+          {xash.from(text || '- - - - - - - - -')}
+        </p>
+
+        <p><b>Check result (copy here)</b></p>
+        <textarea style={{ width: '100%', height: '15vh' }} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/xash.js
@@ -0,0 +1,107 @@
+//@flow
+import * as React from 'react';
+import random from 'lodash/random';
+import times from 'lodash/times';
+
+const initialRobots: string = '(googlebot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)';
+
+type Inputs  = {
+  elementTypes: ?Array<string>,
+  robots: ?string,
+  userAgent: any,
+}
+
+class Xash {
+  elementTypes: Array<string>;
+  robots: string;
+  repeat: number;
+  userAgent: any;
+
+  constructor(inputs: Inputs) {
+    this.elementTypes = (inputs && inputs.elementTypes) || [
+      "b",
+      "big",
+      "i",
+      "small",
+      "tt",
+      "abbr",
+      "acronym",
+      "cite",
+      "code",
+      "dfn",
+      "em",
+      "kbd",
+      "strong",
+      "samp",
+      "time",
+      "var"
+    ];
+
+    this.robots = (inputs && inputs.robots) || initialRobots;
+    this.repeat = 1;
+    if (window && window.navigator && window.navigator.userAgent) {
+      this.userAgent = navigator.userAgent;
+    } else if(inputs && inputs.userAgent) {
+      this.userAgent = inputs && inputs.userAgent;
+    } else {
+      console.error('You are using xash in server side rendering, Please provide userAgent');
+    }
+  }
+
+  set repeats(number: number) {
+    this.repeat = number;
+    return this.repeat;
+  }
+
+  getStyle(): {|style: Object|} {
+    return {
+      style: {
+        display: "inline-block",
+        width: 0,
+        height: 0,
+        opacity: 0,
+        color: 'transparent',
+        zIndex: -99999999,
+        margin: 0,
+        lineHeight: 0,
+        padding: 0,
+      }
+    }
+  }
+
+  elementGenerator (i: string, idx: number, Element: React.Node): React.Fragment {
+    return (
+      <React.Fragment key={idx}>
+        {i}
+        {Element}
+      </React.Fragment>
+    )
+  }
+
+  random(len: number): string {
+    const randomString:string = times(len, () => random(35).toString(36)).join('');
+    return randomString;
+  }
+  
+  from = (text: string) => {
+    const pattern = new RegExp(this.robots, 'i');
+    if (pattern.test(this.userAgent)) return text;
+  
+    const generatedSymbols: string = this.random(text.length * 30);
+  
+    const xashedElements: Array<*> = [].map.call(text, (i: string, idx: number) => {
+      const randomNum: number = random(0, this.elementTypes.length - 1);
+      const hash: string = generatedSymbols[idx * randomNum];
+      const Element = React.createElement(
+        this.elementTypes[randomNum],
+        this.getStyle(),
+        String(hash).repeat(this.repeat)
+      );
+      return this.elementGenerator(i, idx, Element);
+    });
+  
+    return xashedElements;
+  };
+}
+
+export default Xash;
